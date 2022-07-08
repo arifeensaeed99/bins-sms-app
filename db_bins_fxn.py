cca2dbf943a3ab479bdb0c30e34093ea86609653
Arifeen Saeed <arifeensaeed@utexas.edu>
2022-07-08 00:58:54 -0500

    Latest

--- db_bins_fxn.py
+++ db_bins_fxn.py
@@ -6,9 +6,20 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
+import psycopg2
+
+user = 'kkrxhdcnukwpky'
+password = 'b00307276acd85718b22958bc86632f45018449551928445371c2e38c0d9b379'
+host = 'ec2-44-205-41-76.compute-1.amazonaws.com'
+port = '5432'
+database = 'dciocdjj8v1tq5'
+
+conn = psycopg2.connect(database=database, user=user, password=password, host = host, port = port)
+c = conn.cursor()
+
 # link to your database
-engine = create_engine(os.getenv('postgresql-concave-71120'))
-db = scoped_session(sessionmaker(bind = engine))
+# ngine = create_engine(os.getenv('postgres://kkrxhdcnukwpky:b00307276acd85718b22958bc86632f45018449551928445371c2e38c0d9b379@ec2-44-205-41-76.compute-1.amazonaws.com:5432/dciocdjj8v1tq5'))
+# db = scoped_session(sessionmaker(bind = engine))
 # attach the data frame (df) to the database with a name of the 
 # table; the name can be whatever you like
 
@@ -46,22 +57,22 @@ import sqlite3
 
 # Table
 def create_bins_dates_tables():
-    db.execute('CREATE TABLE IF NOT EXISTS bins_table(username TEXT, bin TEXT, bin_completion_date DATE, bin_status BOOL, CONSTRAINT user_bins_pk PRIMARY KEY (username, bin))') # removed date
-    db.execute('PRAGMA foreign_keys = ON')
-    db.execute('CREATE TABLE IF NOT EXISTS bin_dates_table(bin_datestamp DATE, bin_level INTEGER, username TEXT, bin TEXT, FOREIGN KEY(username, bin) REFERENCES bins_table(username, bin) ON UPDATE CASCADE)')
+    c.execute('CREATE TABLE IF NOT EXISTS bins_table(username TEXT, bin TEXT, bin_completion_date DATE, bin_status BOOL, CONSTRAINT user_bins_pk PRIMARY KEY (username, bin))') # removed date
+    c.execute('PRAGMA foreign_keys = ON')
+    c.execute('CREATE TABLE IF NOT EXISTS bin_dates_table(bin_datestamp DATE, bin_level INTEGER, username TEXT, bin TEXT, FOREIGN KEY(username, bin) REFERENCES bins_table(username, bin) ON UPDATE CASCADE)')
 
-    db.commit()
+    c.commit()
 
 # init for both at once
 def add_bin(username, bin, bin_datestamp, bin_level, bin_completion_date, bin_status):
-    db.execute('INSERT INTO bins_table(username, bin, bin_completion_date, bin_status) VALUES (?,?,?,?)', (username, bin, bin_completion_date, bin_status))
-    db.execute('INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (?,?,?,?)', (bin_datestamp, bin_level, username, bin))
+    c.execute('INSERT INTO bins_table(username, bin, bin_completion_date, bin_status) VALUES (?,?,?,?)', (username, bin, bin_completion_date, bin_status))
+    c.execute('INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (?,?,?,?)', (bin_datestamp, bin_level, username, bin))
     
-    db.commit()
+    c.commit()
 
 def add_bin_dates(bin_datestamp, bin_level, username, bin):
-    db.execute('INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (?,?,?,?)', (bin_datestamp, bin_level, username, bin))
-    db.commit()
+    c.execute('INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (?,?,?,?)', (bin_datestamp, bin_level, username, bin))
+    c.commit()
 
 #def view_all_bins_data(username):
 #    c.execute('SELECT bin, bin_date, bin_level, bin_completion_date, bin_status FROM bins_table WHERE username = "{}"'.format(username))
@@ -74,8 +85,8 @@ def add_bin_dates(bin_datestamp, bin_level, username, bin):
 #    return data
 
 def view_all_bins_details(username):
-    db.execute('SELECT bin FROM bins_table WHERE username = ?', (username,)) # no need for DISTINCT
-    data = db.fetchall()
+    c.execute('SELECT bin FROM bins_table WHERE username = ?', (username,)) # no need for DISTINCT
+    data = c.fetchall()
     return data
 
 #def view_all_bins_statuses(username):
@@ -85,20 +96,20 @@ def view_all_bins_details(username):
 
 # details for a given bin
 def get_bin_details_data(bin, username):
-    db.execute('SELECT bin, bin_completion_date, bin_status FROM bins_table WHERE bin  = "{}" AND username = "{}"'.format(bin, username))
-    data = db.fetchall()
+    c.execute('SELECT bin, bin_completion_date, bin_status FROM bins_table WHERE bin  = "{}" AND username = "{}"'.format(bin, username))
+    data = c.fetchall()
     return data
 
 # get all bins dates data
 def get_all_bin_dates_data(username):
-    db.execute('SELECT bin, bin_datestamp, bin_level FROM bin_dates_table WHERE username = "{}"'.format(username))
-    data = db.fetchall()
+    c.execute('SELECT bin, bin_datestamp, bin_level FROM bin_dates_table WHERE username = "{}"'.format(username))
+    data = c.fetchall()
     return data
 
 # get bins dates data for a specific bin
 def get_bin_dates_data(bin, username):
-    db.execute('SELECT bin_datestamp, bin_level, username, bin FROM bin_dates_table WHERE username = "{}" AND bin = "{}"'.format(username, bin))
-    data = db.fetchall()
+    c.execute('SELECT bin_datestamp, bin_level, username, bin FROM bin_dates_table WHERE username = "{}" AND bin = "{}"'.format(username, bin))
+    data = c.fetchall()
     return data
 
 #def get_status_task_notes(note_status, username):
@@ -113,27 +124,27 @@ def get_bin_dates_data(bin, username):
 
 # only changing bin name, completion date, status (Details)
 def update_bin_details(new_bin, new_bin_completion_date, new_bin_status, username, bin):
-    db.execute('UPDATE bins_table SET bin = ?, bin_completion_date = ?, bin_status = ? WHERE username = ? and bin = ?', (new_bin, new_bin_completion_date, new_bin_status, username, bin))
-    db.commit()
-    data = db.fetchall()
+    c.execute('UPDATE bins_table SET bin = ?, bin_completion_date = ?, bin_status = ? WHERE username = ? and bin = ?', (new_bin, new_bin_completion_date, new_bin_status, username, bin))
+    c.commit()
+    data = c.fetchall()
     return data
 
 
 
 # delete all bin_dates and bin_details
 def delete_bins(username, bin):
-    db.execute('DELETE FROM bin_dates_table WHERE bin = ? AND username = ?', (bin, username))
-    db.execute('DELETE FROM bins_table WHERE bin = ? AND username = ?', (bin, username))
-    db.commit()
+    c.execute('DELETE FROM bin_dates_table WHERE bin = ? AND username = ?', (bin, username))
+    c.execute('DELETE FROM bins_table WHERE bin = ? AND username = ?', (bin, username))
+    c.commit()
 
 # Get ALL bins_table data 
 def view_all_bins_table_data():
-    db.execute('SELECT * FROM bins_table')
-    data = db.fetchall()
+    c.execute('SELECT * FROM bins_table')
+    data = c.fetchall()
     return data
 
 # Get ALL bin_dates_table data 
 def view_all_bin_dates_table_data():
-    db.execute('SELECT * FROM bin_dates_table')
-    data = db.fetchall()
+    c.execute('SELECT * FROM bin_dates_table')
+    data = c.fetchall()
     return data
--- db_bins_users.py
+++ db_bins_users.py
@@ -6,9 +6,21 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os
+
+import psycopg2
+
+user = 'kkrxhdcnukwpky'
+password = 'b00307276acd85718b22958bc86632f45018449551928445371c2e38c0d9b379'
+host = 'ec2-44-205-41-76.compute-1.amazonaws.com'
+port = '5432'
+database = 'dciocdjj8v1tq5'
+
+conn = psycopg2.connect(database=database, user=user, password=password, host = host, port = port)
+c = conn.cursor()
+
 # link to your database
-engine = create_engine(os.getenv('postgresql-concave-71120'))
-db = scoped_session(sessionmaker(bind = engine))
+#engine = create_engine(os.getenv(['postgresql-concave-71120']))
+# db = scoped_session(sessionmaker(bind = engine))
 # attach the data frame (df) to the database with a name of the 
 # table; the name can be whatever you like
 
@@ -39,27 +51,27 @@ import sqlite3
     
 # Table
 def create_users_bins_table():
-    db.execute('CREATE TABLE IF NOT EXISTS users_bins_table(username TEXT UNIQUE, password TEXT, email TEXT, phone TEXT UNIQUE, carrier TEXT, timezone TEXT)') # not null
+    c.execute('CREATE TABLE IF NOT EXISTS users_bins_table(username TEXT UNIQUE, password TEXT, email TEXT, phone TEXT UNIQUE, carrier TEXT, timezone TEXT)') # not null
 
-    db.commit()
+    c.commit()
 
 def add_users_bins_data(username, password, email, phone, carrier, timezone):
-    db.execute('INSERT INTO users_bins_table(username, password, email, phone, carrier, timezone) VALUES (?,?,?,?,?, ?)', (username, password, email, phone, carrier, timezone))
-    db.commit()
+    c.execute('INSERT INTO users_bins_table(username, password, email, phone, carrier, timezone) VALUES (?,?,?,?,?, ?)', (username, password, email, phone, carrier, timezone))
+    c.commit()
 
 def login_bins_user(username, password):
-    db.execute('SELECT * FROM users_bins_table WHERE username = ? AND password = ?', (username, password))
-    data = db.fetchall()
+    c.execute('SELECT * FROM users_bins_table WHERE username = ? AND password = ?', (username, password))
+    data = c.fetchall()
     return data
 
 def view_all_bins_users():
-    db.execute('SELECT * FROM users_bins_table')
-    data = db.fetchall()
+    c.execute('SELECT * FROM users_bins_table')
+    data = c.fetchall()
     return data
 
 
 def update_bins_user_data(new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone):
-    db.execute('UPDATE users_bins_table SET username = ?, password = ?, email = ?, phone = ?, carrier = ?, timezone = ? WHERE username = ? and password = ? and email = ? and phone = ? and carrier = ? and timezone = ?', (new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone))
-    db.commit()
-    data = db.fetchall()
+    c.execute('UPDATE users_bins_table SET username = ?, password = ?, email = ?, phone = ?, carrier = ?, timezone = ? WHERE username = ? and password = ? and email = ? and phone = ? and carrier = ? and timezone = ?', (new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone))
+    c.commit()
+    data = c.fetchall()
     return data
