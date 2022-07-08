881b1404ee034adbe87da7b96428fad552f839d2
Arifeen Saeed <arifeensaeed@utexas.edu>
2022-07-08 01:47:10 -0500

    PostgreSQL WOHOO

--- db_bins_users.py
+++ db_bins_users.py
@@ -16,11 +16,8 @@ c = conn.cursor()
 def create_users_bins_table():
     c.execute('CREATE TABLE IF NOT EXISTS users_bins_table(username TEXT UNIQUE, password TEXT, email TEXT, phone TEXT UNIQUE, carrier TEXT, timezone TEXT)') # not null
 
-    c.close()
-
 def add_users_bins_data(username, password, email, phone, carrier, timezone):
     c.execute('INSERT INTO users_bins_table(username, password, email, phone, carrier, timezone) VALUES (?,?,?,?,?, ?)', (username, password, email, phone, carrier, timezone))
-    c.close()
 
 def login_bins_user(username, password):
     c.execute('SELECT * FROM users_bins_table WHERE username = ? AND password = ?', (username, password))
@@ -35,6 +32,5 @@ def view_all_bins_users():
 
 def update_bins_user_data(new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone):
     c.execute('UPDATE users_bins_table SET username = ?, password = ?, email = ?, phone = ?, carrier = ?, timezone = ? WHERE username = ? and password = ? and email = ? and phone = ? and carrier = ? and timezone = ?', (new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone))
-    c.commit()
     data = c.fetchall()
     return data
