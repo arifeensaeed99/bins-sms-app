fc2d3619b4d783e70b95f1e8c649526a8f71b6e5
Arifeen Saeed <arifeensaeed@utexas.edu>
2022-08-14 00:47:04 -0500

    Dates

--- db_bins_fxn.py
+++ db_bins_fxn.py
@@ -12,7 +12,6 @@ database = 'dciocdjj8v1tq5'
 conn = psycopg2.connect(database=database, user=user, password=password, host = host, port = port)
 c = conn.cursor()
 
-
 conn.set_session(autocommit=True) # for errors
 
 # Table
@@ -25,13 +24,13 @@ def create_bins_dates_tables():
 
 # init for both at once
 def add_bin(username, bin, bin_datestamp, bin_level, bin_completion_date, bin_status):
-    c.execute("INSERT INTO bins_table(username, bin, bin_completion_date, bin_status) VALUES (%s,%s,%s,%s);" % (username, bin, bin_completion_date, bin_status))
-    c.execute("INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (%s,%s,%s,%s);" % (bin_datestamp, bin_level, username, bin))
+    c.execute("INSERT INTO bins_table(username, bin, bin_completion_date, bin_status) VALUES (%s,%s,%s,%s);", (username, bin, bin_completion_date, bin_status,))
+    c.execute("INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (%s,%s,%s,%s);", (bin_datestamp, bin_level, username, bin,))
 
     conn.commit()
 
 def add_bin_dates(bin_datestamp, bin_level, username, bin):
-    c.execute("INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (%s,%s,%s,%s);" % (bin_datestamp, bin_level, username, bin))
+    c.execute("INSERT INTO bin_dates_table(bin_datestamp, bin_level, username, bin) VALUES (%s,%s,%s,%s);" , (bin_datestamp, bin_level, username, bin,))
 
     conn.commit()
 
@@ -47,7 +46,7 @@ def add_bin_dates(bin_datestamp, bin_level, username, bin):
 #    return data
 
 def view_all_bins_details(username):
-    c.execute("SELECT bin FROM bins_table WHERE username = %s;" % (username)) # no need for DISTINCT
+    c.execute("SELECT bin FROM bins_table WHERE username = %s;",  (username,)) # no need for DISTINCT
     data = c.fetchall()
     return data
 
@@ -58,19 +57,19 @@ def view_all_bins_details(username):
 
 # details for a given bin
 def get_bin_details_data(bin, username):
-    c.execute("SELECT bin, bin_completion_date, bin_status FROM bins_table WHERE bin  = %s AND username = %s;" % (bin, username))
+    c.execute("SELECT bin, bin_completion_date, bin_status FROM bins_table WHERE bin  = %s AND username = %s;" , (bin, username,))
     data = c.fetchall()
     return data
 
 # get all bins dates data
 def get_all_bin_dates_data(username):
-    c.execute("SELECT bin, bin_datestamp, bin_level FROM bin_dates_table WHERE username = %s;" % (username))
+    c.execute("SELECT bin, bin_datestamp, bin_level FROM bin_dates_table WHERE username = %s;", (username,))
     data = c.fetchall()
     return data
 
 # get bins dates data for a specific bin
 def get_bin_dates_data(bin, username):
-    c.execute("SELECT bin_datestamp, bin_level, username, bin FROM bin_dates_table WHERE username = %s AND bin = %s;" % (username, bin))
+    c.execute("SELECT bin_datestamp, bin_level, username, bin FROM bin_dates_table WHERE username = %s AND bin = %s;", (username, bin,))
     data = c.fetchall()
     return data
 
@@ -86,15 +85,15 @@ def get_bin_dates_data(bin, username):
 
 # only changing bin name, completion date, status (Details)
 def update_bin_details(new_bin, new_bin_completion_date, new_bin_status, username, bin):
-    c.execute("UPDATE bins_table SET bin = %s, bin_completion_date = %s, bin_status = %s WHERE username = %s and bin = %s;" % (new_bin, new_bin_completion_date, new_bin_status, username, bin))
+    c.execute("UPDATE bins_table SET bin = %s, bin_completion_date = %s, bin_status = %s WHERE username = %s and bin = %s;", (new_bin, new_bin_completion_date, new_bin_status, username, bin,))
     # data = c.fetchall()
     conn.commit()
     # return data
 
 # delete all bin_dates and bin_details
 def delete_bins(username, bin):
-    c.execute("DELETE FROM bin_dates_table WHERE bin = %s AND username = %s;" % (bin, username))
-    c.execute("DELETE FROM bins_table WHERE bin = %s AND username = %s;" % (bin, username))
+    c.execute("DELETE FROM bin_dates_table WHERE bin = %s AND username = %s;" , (bin, username,))
+    c.execute("DELETE FROM bins_table WHERE bin = %s AND username = %s;" , (bin, username,))
     conn.commit()
 
 # Get ALL bins_table data 
--- db_bins_users.py
+++ db_bins_users.py
@@ -9,8 +9,6 @@ host = 'ec2-44-205-41-76.compute-1.amazonaws.com'
 port = '5432'
 database = 'dciocdjj8v1tq5'
 
-
-
 conn = psycopg2.connect(database=database, user=user, password=password, host = host, port = port)
 c = conn.cursor()
 
@@ -23,12 +21,12 @@ def create_users_bins_table():
     conn.commit()
 
 def add_users_bins_data(username, password, email, phone, carrier, timezone):
-    c.execute("INSERT INTO users_bins_table(username, password, email, phone, carrier, timezone) VALUES (%s,%s, %s, %s, %s, %s);" % (username, password, email, phone, carrier, timezone))
+    c.execute("INSERT INTO users_bins_table(username, password, email, phone, carrier, timezone) VALUES (%s,%s, %s, %s, %s, %s);", (username, password, email, phone, carrier, timezone,))
     
     conn.commit()
 
 def login_bins_user(username, password):
-    c.execute("SELECT * FROM users_bins_table WHERE username = %s AND password = %s;" % (username, password))
+    c.execute("SELECT * FROM users_bins_table WHERE username = %s AND password = %s;", (username, password,))
     data = c.fetchall()
     return data
 
@@ -38,7 +36,7 @@ def view_all_bins_users():
     return data
 
 def update_bins_user_data(new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone):
-    c.execute("UPDATE users_bins_table SET username = %s, password = %s, email = %s, phone = %s, carrier = %s, timezone = %s WHERE username = %s and password = %s and email = %s and phone = %s and carrier = %s and timezone = %s;" % (new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone))
+    c.execute("UPDATE users_bins_table SET username = %s, password = %s, email = %s, phone = %s, carrier = %s, timezone = %s WHERE username = %s and password = %s and email = %s and phone = %s and carrier = %s and timezone = %s;", (new_username, new_password, new_email, new_phone, new_carrier, new_timezone, username, password, email, phone, carrier, timezone,))
     
     conn.commit()
 
